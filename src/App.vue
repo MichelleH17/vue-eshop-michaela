056359d718a139387d562c21201790ab2accb8b6
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-03 11:06:40 +0100

    fix: rename routes and nav links, add redirect,

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/">Store</RouterLink>
+      <RouterLink to="/cart">Cart</RouterLink>
     </nav>
   </header>
 
