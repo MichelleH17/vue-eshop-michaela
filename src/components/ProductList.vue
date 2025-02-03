53007dce9a2c75d1cd8d1120402aabfd6336f82b
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-03 15:13:25 +0100

    feat: add ProductItem component to display products

---
+++ src/components/ProductList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <ProductItem v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useProductStore } from '@/stores/product'
+import ProductItem from './ProductItem.vue'
+
+const { products } = useProductStore()
+</script>
