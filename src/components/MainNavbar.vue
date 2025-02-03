dcaed7086c059e32aa5bd365bdfb8a2263582d3c
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-03 13:36:53 +0100

    fix: use useRoute

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -3,13 +3,15 @@
     <nav class="flex items-center justify-between px-4">
       <RouterLink to="/store" class="hover:bg-green-800 px-2 py-0.5 rounded">E-SHOP</RouterLink>
       <div class="flex space-x-4">
-        <RouterLink to="/" class="hover:bg-green-800 px-2 py-0.5 rounded" :class="{'bg-green-800': $route.path === '/store'}">Store</RouterLink>
-        <RouterLink to="/cart" class="hover:bg-green-800 px-2 py-0.5 rounded" :class="{ 'bg-green-800': $route.path === '/cart'}">Cart</RouterLink>
+        <RouterLink to="/" class="hover:bg-green-800 px-2 py-0.5 rounded" :class="{'bg-green-800': route.path === '/store'}">Store</RouterLink>
+        <RouterLink to="/cart" class="hover:bg-green-800 px-2 py-0.5 rounded" :class="{ 'bg-green-800': route.path === '/cart'}">Cart</RouterLink>
       </div>
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute } from 'vue-router'
+
+const route = useRoute()
 </script>
