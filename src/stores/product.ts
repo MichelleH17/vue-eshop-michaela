7c204e811b77e405722023f9f248cb8abd5e60fd
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-03 16:54:39 +0100

    fix: use interface Product, change json name

--- src/stores/product.ts
+++ src/stores/product.ts
@@ -3,11 +3,11 @@ import { defineStore } from 'pinia'
 import type { Product } from '../types'
 
 export const useProductStore = defineStore('product', () => {
-  const products = ref([] as Product[])
+  const products = ref<Product[]>([])
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch("/shop-item.json")
+      const response = await fetch("src/shop-items.json")
       products.value = await response.json()
     } catch (error) {
       console.error("Failed to load products:", error)
