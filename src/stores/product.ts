52df5e775be8b0be766246104048d2102d24b38e
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-04 11:25:54 +0100

    feat: add function for adding and decreasing quantity

--- src/stores/product.ts
+++ src/stores/product.ts
@@ -4,6 +4,7 @@ import type { Product } from '../types'
 
 export const useProductStore = defineStore('product', () => {
   const products = ref<Product[]>([])
+  const cartItems = ref<{ product: Product, quantity: number }[]>([])
 
   const fetchProducts = async () => {
     try {
@@ -14,5 +15,29 @@ export const useProductStore = defineStore('product', () => {
     }
   }
 
-  return { products, fetchProducts }
+  const addItem = (product: Product) => {
+    const itemInCart = cartItems.value.find(item => item.product.id === product.id)
+    if (itemInCart) {
+      itemInCart.quantity++
+    } else {
+      cartItems.value.push({ product, quantity: 1 })
+    }
+  }
+
+  const increaseQuantity = (product: Product) => {
+    const itemInCart = cartItems.value.find(item => item.product.id === product.id)
+    if (itemInCart) {
+      itemInCart.quantity++
+    }
+  }
+
+  const decreaseQuantity = (product: Product) => {
+    const itemInCart = cartItems.value.find(item => item.product.id === product.id)
+    if (itemInCart && itemInCart.quantity > 1) {
+      itemInCart.quantity--
+      }
+    }
+
+
+  return { products, fetchProducts, cartItems, addItem, increaseQuantity, decreaseQuantity }
 })
