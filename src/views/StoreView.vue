42062f04d578960c48e1a3bdf23a1bcc62e9c5ea
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-04 23:55:26 +0100

    feat: add CartIcon component to StoreView

--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,8 +1,10 @@
 <template>
   <ProductList />
+  <CartIcon />
 </template>
 
 <script setup lang="ts">
 import ProductList from '@/components/ProductList.vue'
+import CartIcon from '@/components/CartIcon.vue'
 </script>
 
