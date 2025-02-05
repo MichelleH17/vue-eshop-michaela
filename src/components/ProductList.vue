db70f3fcd47291032dfb2d9bcab3f959f4c65980
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-05 10:00:13 +0100

    fix: add bottom padding to ProductList container

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto px-4">
+  <div class="container mx-auto px-4 pb-40">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
       <ProductItem v-for="product in products" :key="product.id" :product="product" />
     </div>
