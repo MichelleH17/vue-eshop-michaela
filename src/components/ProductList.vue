a51ef9619f955b86b18d797cf0b13383700e8ff7
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-05 09:57:08 +0100

    feat: enhance ProductList layout

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-    <ProductItem v-for="product in products" :key="product.id" :product="product" />
+  <div class="container mx-auto px-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+      <ProductItem v-for="product in products" :key="product.id" :product="product" />
+    </div>
   </div>
 </template>
 
