21f4a2ba7ccd35ad1215886539de77e76d570a6a
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-06 16:58:25 +0100

    fix: update MainNavbar links and styles

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,12 +1,17 @@
 <template>
   <header class="sticky top-0 left-0 w-full bg-green-700 text-white text-2xl py-2">
     <nav class="flex items-center justify-between px-4">
-      <RouterLink to="/store" class="hover:bg-green-800 px-2 py-0.5 rounded">E-SHOP</RouterLink>
+      <RouterLink to="/" class="hover:bg-green-800 px-2 py-0.5 rounded">E-SHOP</RouterLink>
       <div class="flex space-x-4">
-        <RouterLink to="/" class="hover:bg-green-800 px-2 py-0.5 rounded" :class="{'bg-green-800': route.path === '/store'}">Store</RouterLink>
-        <RouterLink to="/cart" class="hover:bg-green-800 px-2 py-0.5 rounded" :class="{ 'bg-green-800': route.path === '/cart'}">
+        <RouterLink to="/store" class="hover:bg-green-800 px-2 py-0.5 rounded">Store</RouterLink>
+        <RouterLink to="/orders" class="hover:bg-green-800 px-2 py-0.5 rounded">Orders</RouterLink>
+        <RouterLink to="/cart" class="hover:bg-green-800 px-2 py-0.5 rounded">
           Cart
-          <span v-if="totalItems > 0" class="absolute top-4 right-4 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs font-semibold">{{ totalItems }}</span>
+          <span
+            v-if="totalItems > 0"
+            class="absolute top-4 right-4 transform translate-x-1/2 -translate-y-1/2 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs font-semibold"
+            >{{ totalItems }}</span
+          >
         </RouterLink>
       </div>
     </nav>
@@ -15,10 +20,15 @@
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { RouterLink, useRoute } from 'vue-router'
+import { RouterLink } from 'vue-router'
 import { useProductStore } from '@/stores/product'
 
-const route = useRoute()
 const productStore = useProductStore()
 const totalItems = computed(() => productStore.totalItems)
 </script>
+
+<style scoped>
+.router-link-exact-active {
+  background-color: #016630;
+}
+</style>
