029948ed579463b7fbbd9437535db76a769caa18
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-06 16:57:27 +0100

    feat: create OrderCard and OrderList components to manage orders

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+  Order card
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/components/OrderList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+  Order list
+  </div>
+  <OrderCard />
+</template>
+
+<script setup lang="ts">
+import OrderCard from './OrderCard.vue'
+</script>
+
