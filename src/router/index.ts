7609ff3b61ef628f73076c37769327c79cb5b520
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-06 16:58:54 +0100

    feat: add orders route to the router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import StoreView from '@/views/StoreView.vue'
 import CartView from '@/views/CartView.vue'
+import OrdersView from '@/views/OrdersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,11 @@ const router = createRouter({
       path: '/cart',
       name: 'cart',
       component: CartView,
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: OrdersView,
     },
   ],
 })
