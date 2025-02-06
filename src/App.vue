1368994b568022172fedec1fe4855c1771f27851
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-06 17:00:17 +0100

    fix: change order of template and script

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
+<template>
+  <MainNavbar />
+  <RouterView />
+</template>
+
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import MainNavbar from './components/MainNavbar.vue'
 </script>
 
-<template>
-  <MainNavbar />
-  <RouterView />
-</template>
