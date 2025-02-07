2153014edfc40ab68c9c0d554866467660823d2c
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-07 13:29:12 +0100

    feat: bind orders to OrderList component in OrdersView

--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,8 +1,15 @@
 <template>
-  <OrderList />
+  <div>
+    <OrderList :orders="orders" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import OrderList from '@/components/OrderList.vue'
+import { useProductStore } from '@/stores/product'
+import { computed } from 'vue';
 
+const productStore = useProductStore();
+
+const orders = computed(() => productStore.orders)
 </script>
