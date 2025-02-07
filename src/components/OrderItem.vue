a009b3cdcab6a6aec7a88be2856e2ac3a591fb3c
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-07 13:32:35 +0100

    feat: add OrderItem component to display individual order details

---
+++ src/components/OrderItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li
+    class="grid grid-cols-5 gap-4 items-center m-4 border border-gray-200 rounded-lg p-4 shadow-md"
+  >
+    <img :src="picture" alt="Product Image" class="w-16 h-16 object-cover col-span-1" />
+    <h2 class="text-lg font-semibold col-span-2">{{ item.product.name }}</h2>
+    <p class="text-lg col-span-1 justify-self-end">
+      {{ item.quantity }}x {{ item.product.price }}€
+    </p>
+    <p class="text-lg font-semibold col-span-1 justify-self-end">{{ totalPriceForProduct }}€</p>
+  </li>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+  picture: string
+  item: { product: { name: string; price: number }; quantity: number }
+}>()
+
+const totalPriceForProduct = props.item.product.price * props.item.quantity
+</script>
