70925a154f157c85af6620509f94d44dd7d2eec7
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-07 13:31:40 +0100

    feat: update OrderList component to display orders with OrderItem and routing

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-  Order list
-  </div>
-  <OrderCard />
+  <ul>
+    <li v-for="(order, index) in orders" :key="index">
+      <RouterLink :to="`/orders/${index}`">
+        <OrderItem
+          v-for="(item, itemIndex) in order"
+          :key="itemIndex"
+          :picture="getProductDetails(item.productId)?.picture || ''"
+          :item="{ product: { name: getProductDetails(item.productId)?.name || '', price: getProductDetails(item.productId)?.price || 0 }, quantity: item.quantity }"
+        />
+      </RouterLink>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import OrderCard from './OrderCard.vue'
-</script>
+import OrderItem from '@/components/OrderItem.vue'
+import { useProductStore } from '@/stores/product'
+import { RouterLink } from 'vue-router'
 
+defineProps<{
+  orders: Array<{ productId: number; quantity: number }[]>
+}>()
+
+const productStore = useProductStore()
+
+const getProductDetails = (productId: number) => {
+  return productStore.getProductDetails(productId)
+}
+</script>
