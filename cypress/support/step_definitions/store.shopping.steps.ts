4d39e410fb6d9efb5acc8827c08330b0db204dd2
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-12 11:22:50 +0100

    feat: add step definitions for cart quantity manipulation in shopping steps

---
+++ cypress/support/step_definitions/store.shopping.steps.ts
@@ -0,0 +1,9 @@
+import { Given, When, Then, DataTable } from "@badeball/cypress-cucumber-preprocessor";
+
+Then(/the cart icon should contain "(.+)"/, (quantity: number) => {
+  cy.get("div span").contains(quantity);
+})
+
+When(/the user decreases the quantity of "(.+)"/, (productName: string) => {
+  cy.contains(productName).closest("div").find("button").contains("-").click();
+})
